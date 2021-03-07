f57efa6faf1db8c020e513b0057c288734c56ec1
lukmanhidayah <lukmanhidayah01@gmail.com>
2021-03-07 22:56:45 +0700

    Update redirect to home

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 import Header from "./components/header/Header";
 import SignInSignUp from "./components/sign-in-sign-up/SignInSignUp";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
 import { setCurrentUser } from "./redux/user/userAction";
 function App() {
   const dispatch = useDispatch();
+
+  const currentUser = useSelector((state) => state.user.currentUser);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
@@ -43,7 +45,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInSignUp} />
+        <Route
+          path="/signin"
+          render={() => (currentUser ? <Redirect to="/" /> : <SignInSignUp />)}
+        />
       </Switch>
     </div>
   );
